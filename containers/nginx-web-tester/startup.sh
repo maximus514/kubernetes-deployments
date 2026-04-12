ad0de4571eb0e22742c436d16f257b15eed2e223
Maximus <maximus.primus.of.the.north@gmail.com>
2026-04-12 13:48:51 -0400

    added more metadata to the HTML page, including the VM hostname, DNS resolution, IP address, pod hostname, and route information. This allows for better visibility into the environment in which the Nginx container is running. Additionally, updated the Dockerfile to install net-tools for retrieving route information.

--- containers/nginx-web-tester/startup.sh
+++ containers/nginx-web-tester/startup.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 # Fetch metadata
-HOSTNAME=$(curl -s -H "Metadata-Flavor: Google" \
+VMHOSTNAME=$(curl -s -H "Metadata-Flavor: Google" \
   http://metadata.google.internal/computeMetadata/v1/instance/hostname)
 
+DNSRESOLUTION=$(cat /etc/resolv.conf)
+
+PODHOSTNAME=$(cat /etc/hostname)
+
+IPADDRESS=$(cat /etc/hosts)
+
+Route=$(route)
+
 # Export for envsubst
-export HOSTNAME
+export VMHOSTNAME
+export DNSRESOLUTION
+export PODHOSTNAME
+export IPADDRESS
+export Route
+
 
 # Replace variables in template → final HTML
 envsubst < /usr/share/nginx/html/index.html.template \
